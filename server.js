cb2ef1a825f5160351dddbfc89a5a5cf8f62549b
EGGabriel <ps.eggabriel@gmail.com>
2017-12-15 17:28:50 -0200

    ADD ALL Dependencies

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
+//Dependencias
 const express = require('express')
 const app = express()
 const path = require('path')
 const PORT = 3000
-const axios = require('axios')
 
+//Express Configuration
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'))
 
+//Ejs Configuration
 app.set('views', path.join(__dirname, 'public'))
 app.set('view engine', 'ejs')
 
-app.get('/', (request, response) => {
+app.get('/', (request,response) =>{
     response.render('index')
 })
 
